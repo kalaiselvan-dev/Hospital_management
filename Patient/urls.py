c62f64ab3aedad33b41337fa8d983606ff6a3d66
kalaiselvan-dev <90950928+kalaiselvan-dev@users.noreply.github.com>
2022-02-21 23:04:34 +0530

    Add files via upload

---
+++ Patient/urls.py
@@ -0,0 +1,11 @@
+# from .views import RegisterAPI
+# from django.urls import path
+# from knox import views as knox_views
+# from .views import LoginAPI
+
+# urlpatterns = [
+#     path('patient/register/', RegisterAPI.as_view(), name='register'),
+#     path('patient/login/', LoginAPI.as_view(), name='login'),
+#     path('patient/logout/', knox_views.LogoutView.as_view(), name='logout'),
+#     path('patient/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+# ]
